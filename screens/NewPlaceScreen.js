71dfdfed6b460585b8765bc5e0a6952c4c145762
Jason Huels <jasonhuels@yahoo.com>
2020-01-13 12:11:35 -0800

    Complete lesson 2, add text input to new place, install redux

--- screens/NewPlaceScreen.js
+++ screens/NewPlaceScreen.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { UseState } from 'react';
 import { View, TextInput, Text, StyleSheet, ScrollView, Button } from "react-native";
 // import { ScrollView } from 'react-native-gesture-handler';
 import Colors from "../constants/Colors";
 
 const NewPlaceScreen = props => {
+  const [titleValue, setTitleValue] = useState('');
+
+  const titleChangeHandler = text => {
+    setTitleValue(text);
+  };
+
+  const savePlaceHandler = () => {
+    
+  };
+
   return (
     <ScrollView>
-      <View>
-        <Text>Title</Text>
-        <TextInput />
-        <Button title="Save Place" color={Colors.primary} onPress={() => {}} />
+      <View style={styles.form}>
+        <Text style={styles.label}>Title</Text>
+        <TextInput style={styles.textInput} onChangeText={titleChangeHandler} value={titleValue}/>
+        <Button title="Save Place" color={Colors.primary} onPress={savePlaceHandler} />
       </View>
     </ScrollView>
   );
@@ -19,6 +29,21 @@ NewPlaceScreen.navigationOptions = {
   headerTitle: "Add Place"
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  form: {
+    margin: 30
+  },
+  label: {
+    fontSize: 18,
+    marginBottom: 15
+  },
+  textInput: {
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 1,
+    marginBottom: 15,
+    paddingVertical: 4,
+    paddingHorizontal: 2
+  }
+});
 
 export default NewPlaceScreen;
