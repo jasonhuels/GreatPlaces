1aed4ac8c015ac67b5965da71222a12ee3748553
Jason Huels <jasonhuels@yahoo.com>
2020-01-14 10:35:44 -0800

    Complete lesson 4, adding camera permissions

--- screens/NewPlaceScreen.js
+++ screens/NewPlaceScreen.js
@@ -4,6 +4,7 @@ import { View, TextInput, Text, StyleSheet, ScrollView, Button } from "react-nat
 import Colors from "../constants/Colors";
 import { useDispatch } from 'react-redux';
 import * as placesActions from "../store/places-actions";
+import ImagePicker from "../components/ImagePicker";
 
 const NewPlaceScreen = props => {
   const [titleValue, setTitleValue] = useState('');
@@ -24,6 +25,7 @@ const NewPlaceScreen = props => {
       <View style={styles.form}>
         <Text style={styles.label}>Title</Text>
         <TextInput style={styles.textInput} onChangeText={titleChangeHandler} value={titleValue}/>
+        <ImagePicker />
         <Button title="Save Place" color={Colors.primary} onPress={savePlaceHandler} />
       </View>
     </ScrollView>
