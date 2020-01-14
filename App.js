1f5436d65b66e8f16f361afb5bcb2ab9985551dc
Jason Huels <jasonhuels@yahoo.com>
2020-01-14 12:53:32 -0800

    complete lesson 8, initializing sqlite database

--- App.js
+++ App.js
@@ -5,6 +5,14 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import ReduxThunk from 'redux-thunk';
 import placesReducer from "./store/places-reducers"
+import { init } from "./helpers/db";
+
+init().then(() => {
+  console.log("Initialized database");
+}).catch(err => {
+  console.log("Initializing db failed");
+  console.log(err);
+});
 
 const rootReducer = combineReducers({
   places: placesReducer
