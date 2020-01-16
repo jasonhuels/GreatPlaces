756affdfa025246db5fd00ecc1e09d687f5ee0a5
Jason Huels <jasonhuels@yahoo.com>
2020-01-16 13:11:26 -0800

    complete lesson 17, update location screen when the location changes

--- components/LocationPicker.js
+++ components/LocationPicker.js
@@ -11,11 +11,14 @@ const LocationPicker = props => {
 
   const mapPickedLocation = props.navigation.getParam("pickedLocation");
 
+  const {onLocationPicked} = props;
+
   useEffect(() => {
     if(mapPickedLocation) {
       setPickedLocation(mapPickedLocation);
+      onLocationPicked(mapPickedLocation);
     }
-  });
+  }, [mapPickedLocation, onLocationPicked]);
 
   const getLocationHandler = async () => {
     const hasPermission = await verifyPermission();
@@ -28,6 +31,10 @@ const LocationPicker = props => {
       const location = await Location.getCurrentPositionAsync({ timeout: 5000});
       console.log(location);
       setPickedLocation({
+        lat: location.coords.latitude,
+        lng: location.coords.longitude
+      });
+      props.onLocationPicked({
         lat: location.coords.latitude,
         lng: location.coords.longitude
       });
--- screens/NewPlaceScreen.js
+++ screens/NewPlaceScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import { View, TextInput, Text, StyleSheet, ScrollView, Button } from "react-native";
 // import { ScrollView } from 'react-native-gesture-handler';
 import Colors from "../constants/Colors";
@@ -10,6 +10,7 @@ import LocationPicker from "../components/LocationPicker";
 const NewPlaceScreen = props => {
   const [titleValue, setTitleValue] = useState('');
   const [selectedImage, setSelectedImage] = useState();
+  const [selectedLocation, setSelectedLocation] = useState();
 
   const dispatch = useDispatch();
 
@@ -20,9 +21,13 @@ const NewPlaceScreen = props => {
   const imageTakenHandler = imagePath => {
     setSelectedImage(imagePath);
   };
+  
+  const locationPickedHandler = useCallback(location => {
+    setSelectedLocation(location)
+  }, [setSelectedLocation]);
 
   const savePlaceHandler = () => {
-    dispatch(placesActions.addPlace(titleValue, selectedImage));
+    dispatch(placesActions.addPlace(titleValue, selectedImage, selectedLocation));
     props.navigation.goBack();
   };
 
@@ -32,7 +37,7 @@ const NewPlaceScreen = props => {
         <Text style={styles.label}>Title</Text>
         <TextInput style={styles.textInput} onChangeText={titleChangeHandler} value={titleValue}/>
         <ImagePicker onImageTaken={imageTakenHandler} />
-        <LocationPicker navigation={props.navigation} />
+        <LocationPicker navigation={props.navigation} onLocationPicked={locationPickedHandler} />
         <Button title="Save Place" color={Colors.primary} onPress={savePlaceHandler} />
       </View>
     </ScrollView>
